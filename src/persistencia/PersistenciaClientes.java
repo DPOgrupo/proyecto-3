b16ac2e56d85519a7ccbaba4eade3e96936efc54
sant-iago <sa.rojasm1@uniandes.edu.co>
2025-04-20 21:44:24 -0500

    persi tencia

--- src/persistencia/PersistenciaClientes.java
+++ src/persistencia/PersistenciaClientes.java
@@ -7,13 +7,20 @@ import Venta_de_tiquetes.AdministradorTiquetes;
 
 public class PersistenciaClientes {
 
-	private static final String rutaArchivo = "clientes.csv";
+	
+	private final String rutaArchivo = "data/clientes.csv";
 	private AdministradorTiquetes admin; 
 
+	public PersistenciaClientes(AdministradorTiquetes admin) {
+		this.admin = admin; 
+	}
 	
     public void guardarClientes() {
-        
+    	//System.out.println("Clientes encontrados en admin: " + admin.getClientesRegistrados());
+    	System.out.println("Guardando clientes en: " + rutaArchivo);
+    	System.out.println("Total clientes: " + admin.getClientesRegistrados().size());
     	try (PrintWriter writer = new PrintWriter(new FileWriter(rutaArchivo))) {
+    		System.out.println("Ruta absoluta del archivo: " + new File(rutaArchivo).getAbsolutePath());
             for (Cliente c : admin.getClientesRegistrados()) {
                 writer.println(String.join(",", 
                     c.getLogin(), 
@@ -24,6 +31,7 @@ public class PersistenciaClientes {
                 ));
             }
             System.out.println("Clientes guardados correctamente.");
+            //System.out.println("Clientes encontrados en admin: " + admin.getClientesRegistrados());
         } 
         
         catch (IOException e) {
@@ -39,14 +47,15 @@ public class PersistenciaClientes {
             String linea;
             while ((linea = reader.readLine()) != null) {
                 String[] partes = linea.split(",");
-                if (partes.length == 5) {
+                if (partes.length == 6) {
                     String login = partes[0];
                     String nombre = partes[1];
                     String contrasena = partes[2];
                     int dinero = Integer.parseInt(partes[3]);
-                    boolean restSalud = Boolean.parseBoolean(partes[4]);
+                    int edad = Integer.parseInt(partes[4]);
+                    boolean restSalud = Boolean.parseBoolean(partes[5]);
 
-                    Cliente c = new Cliente(login, nombre, contrasena, dinero, restSalud);
+                    Cliente c = new Cliente(login, nombre, contrasena, dinero, edad, restSalud);
                     clientes.add(c);
                 }
             }
